9ae6261c595d22a01022156bb1a2b35712729a40
unknown <lucasaraujoo44.la@gmail.com>
2023-07-30 12:19:20 -0300

    Feat: Finalizando as telas do app

--- Downloads/apiFinal/biblioteca/livros/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('adicionar/', views.adicionar, name='adicionar'),
-    path('editar/<str:id>/', views.editar, name='editar'),
-    path('deletar/<str:id>/', views.deletar, name='deletar'),
-]
